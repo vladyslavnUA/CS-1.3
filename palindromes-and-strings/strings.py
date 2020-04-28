3d28d49005c94cc7243a2a726b94c13ccf4582c2
vladyslavnUA <vladyslavn@yahoo.com>
2020-04-28 00:02:46 -0700

    updated contains

--- palindromes-and-strings/strings.py
+++ palindromes-and-strings/strings.py
@@ -5,8 +5,22 @@ def contains(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement contains here (iteratively and/or recursively)
+    res_data = []
+    # check for string
+    if pattern == "":
+        return True
 
-
+    for index in range(0, len(text)):
+        if text[index] == pattern[0]:   # check if text character aligns with its' index
+            store_index = index   
+            for a in range(len(pattern)):
+                if store_index > len(text) - 1 or pattern[a] != text[store_index]:
+                    break
+                if a == len(pattern) - 1:
+                    res_data.append[index]
+                store_index += 1
+    return len(res_data) != 0
+    
 def find_index(text, pattern):
     """Return the starting index of the first occurrence of pattern in text,
     or None if not found."""
