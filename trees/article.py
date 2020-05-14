77bea8de9139387b9c5c8ed5a24918c023173ffb
vladyslav nykoliuk <37787869+vladyslavnUA@users.noreply.github.com>
2020-05-14 15:46:19 -0700

    updated

--- trees/article.py
+++ trees/article.py
@@ -12,3 +12,16 @@ def insert(self, item):
         elif parent.data < item:
             parent.right = BinaryTreeNode(item)
         self.size += 1
+
+def _find_node_iterative(self, item):
+        node = self.root
+        # Loop until we descend past the closest leaf node
+        while node is not None:
+            if item == node.data:
+                return node
+            elif item < node.data:
+                node = node.left
+            elif item > node.data:
+                node = node.right
+        # Not found
+        return None
