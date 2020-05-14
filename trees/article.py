ce81a9730252a3b0c968b3d53b577f2b9a510bc2
vladyslav nykoliuk <37787869+vladyslavnUA@users.noreply.github.com>
2020-05-14 15:44:06 -0700

    updated

--- trees/article.py
+++ trees/article.py
@@ -1,1 +1,14 @@
-
+def insert(self, item):
+        if self.is_empty():
+            self.root = BinaryTreeNode(item)
+            self.size += 1
+            return
+        parent = self._find_parent_node_recursive(item, self.root)
+        
+        if parent == None:
+            self.root.left = BinaryTreeNode(item)
+        elif parent.data > item:
+            parent.left = BinaryTreeNode(item)
+        elif parent.data < item:
+            parent.right = BinaryTreeNode(item)
+        self.size += 1
